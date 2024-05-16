ea1441a0a0efa3b3715a06209c0788eb879a2702
Alex Nefedov <nefaleksandr8@gmail.com>
2024-05-17 00:09:18 +0300

    #8 Test. Make keyboard bigger

--- scripts/keyboard.gd
+++ scripts/keyboard.gd
@@ -3,11 +3,11 @@ extends Node
 var key_dict = {}
 
 func load_buttons():
-	var file = FileAccess.open("res://keyboard.csv", FileAccess.READ)
+	var file = FileAccess.open("res://texts/keyboard.txt", FileAccess.READ)
 	
 	while not file.eof_reached(): 
 		var line = file.get_line()
-		var values = line.split(",,")
+		var values = line.split("^")
 		if values.size() < 3:
 			break
 		key_dict[values[0]] = Vector2(int(values[1]),int(values[2]))
